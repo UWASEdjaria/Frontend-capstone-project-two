ce8204d271f170b828dfbf1e8657ec36dbe8983c
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-18 10:27:39 +0200

    add page to lab1

---
+++ src/app/lab1/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold mb-4 text-orange-500">
+      Home stories and ideas
+      </h1>
+      <p className="text-lg text-gray-700">
+       <p>you can read ,post your idea ,like and comment</p>
+      </p>
+    </div>
+  );
+}
