aa0f41e8227ecf551aa037cb2866efe4c20e47ff
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-22 17:46:47 +0200

    update

--- src/app/api/lab9/followers/[id]/route.ts
+++ src/app/api/lab9/followers/[id]/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function GET(req: Request, { params }: any) {
   const count = await prisma.followLab9.count({
     where: { followingId: Number(params.id) },
